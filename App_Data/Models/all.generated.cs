258c648256e3012abc0a41ebb172cc585ab23868
Gabor Molnar <gabormolnar@redweb.com>
2017-07-03 19:57:23 +0100

    Finished Header section

--- App_Data/Models/all.generated.cs
+++ App_Data/Models/all.generated.cs
@@ -8,8 +8,8 @@ using  Umbraco.Web;
 using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
-[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "4d04a973ddf7da34")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "ec030b16eb387dda")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
 
 
 // FILE: models.generated.cs
@@ -42,7 +42,7 @@ namespace Umbraco.Web.PublishedContentModels
 {
 	/// <summary>Home</summary>
 	[PublishedContentModel("home")]
-	public partial class Home : PublishedContentModel
+	public partial class Home : PublishedContentModel, IContactInfo
 	{
 #pragma warning disable 0109 // new is redundant
 		public new const string ModelTypeAlias = "home";
@@ -66,12 +66,66 @@ namespace Umbraco.Web.PublishedContentModels
 		}
 
 		///<summary>
-		/// Hide node from navigaiton
+		/// Address line 1: Please provide the first line of your address
 		///</summary>
-		[ImplementPropertyType("UmbracoNaviHide")]
-		public bool UmbracoNaviHide
+		[ImplementPropertyType("addressLine1")]
+		public string AddressLine1
 		{
-			get { return this.GetPropertyValue<bool>("UmbracoNaviHide"); }
+			get { return Umbraco.Web.PublishedContentModels.ContactInfo.GetAddressLine1(this); }
+		}
+
+		///<summary>
+		/// Address line 2: Please provide your second line of your address
+		///</summary>
+		[ImplementPropertyType("addressLine2")]
+		public string AddressLine2
+		{
+			get { return Umbraco.Web.PublishedContentModels.ContactInfo.GetAddressLine2(this); }
+		}
+
+		///<summary>
+		/// Closing Time: Please provide the day when you are not open
+		///</summary>
+		[ImplementPropertyType("closingTime")]
+		public string ClosingTime
+		{
+			get { return Umbraco.Web.PublishedContentModels.ContactInfo.GetClosingTime(this); }
+		}
+
+		///<summary>
+		/// Opening Times: Please provide your opening times
+		///</summary>
+		[ImplementPropertyType("openingTimes")]
+		public string OpeningTimes
+		{
+			get { return Umbraco.Web.PublishedContentModels.ContactInfo.GetOpeningTimes(this); }
+		}
+
+		///<summary>
+		/// Phone number: Please provide your phone number
+		///</summary>
+		[ImplementPropertyType("phoneNumber")]
+		public string PhoneNumber
+		{
+			get { return Umbraco.Web.PublishedContentModels.ContactInfo.GetPhoneNumber(this); }
+		}
+
+		///<summary>
+		/// Postcode: Please provide your Postcode
+		///</summary>
+		[ImplementPropertyType("postcode")]
+		public string Postcode
+		{
+			get { return Umbraco.Web.PublishedContentModels.ContactInfo.GetPostcode(this); }
+		}
+
+		///<summary>
+		/// Text for phone number: Please add some text to you phone number
+		///</summary>
+		[ImplementPropertyType("textForPhoneNumber")]
+		public string TextForPhoneNumber
+		{
+			get { return Umbraco.Web.PublishedContentModels.ContactInfo.GetTextForPhoneNumber(this); }
 		}
 	}
 
@@ -151,6 +205,142 @@ namespace Umbraco.Web.PublishedContentModels
 		{
 			return PublishedContentModelUtility.GetModelPropertyType(GetModelContentType(), selector);
 		}
+	}
+
+	// Mixin content Type 1103 with alias "contactInfo"
+	/// <summary>Contact info</summary>
+	public partial interface IContactInfo : IPublishedContent
+	{
+		/// <summary>Address line 1</summary>
+		string AddressLine1 { get; }
+
+		/// <summary>Address line 2</summary>
+		string AddressLine2 { get; }
+
+		/// <summary>Closing Time</summary>
+		string ClosingTime { get; }
+
+		/// <summary>Opening Times</summary>
+		string OpeningTimes { get; }
+
+		/// <summary>Phone number</summary>
+		string PhoneNumber { get; }
+
+		/// <summary>Postcode</summary>
+		string Postcode { get; }
+
+		/// <summary>Text for phone number</summary>
+		string TextForPhoneNumber { get; }
+	}
+
+	/// <summary>Contact info</summary>
+	[PublishedContentModel("contactInfo")]
+	public partial class ContactInfo : PublishedContentModel, IContactInfo
+	{
+#pragma warning disable 0109 // new is redundant
+		public new const string ModelTypeAlias = "contactInfo";
+		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
+#pragma warning restore 0109
+
+		public ContactInfo(IPublishedContent content)
+			: base(content)
+		{ }
+
+#pragma warning disable 0109 // new is redundant
+		public new static PublishedContentType GetModelContentType()
+		{
+			return PublishedContentType.Get(ModelItemType, ModelTypeAlias);
+		}
+#pragma warning restore 0109
+
+		public static PublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<ContactInfo, TValue>> selector)
+		{
+			return PublishedContentModelUtility.GetModelPropertyType(GetModelContentType(), selector);
+		}
+
+		///<summary>
+		/// Address line 1: Please provide the first line of your address
+		///</summary>
+		[ImplementPropertyType("addressLine1")]
+		public string AddressLine1
+		{
+			get { return GetAddressLine1(this); }
+		}
+
+		/// <summary>Static getter for Address line 1</summary>
+		public static string GetAddressLine1(IContactInfo that) { return that.GetPropertyValue<string>("addressLine1"); }
+
+		///<summary>
+		/// Address line 2: Please provide your second line of your address
+		///</summary>
+		[ImplementPropertyType("addressLine2")]
+		public string AddressLine2
+		{
+			get { return GetAddressLine2(this); }
+		}
+
+		/// <summary>Static getter for Address line 2</summary>
+		public static string GetAddressLine2(IContactInfo that) { return that.GetPropertyValue<string>("addressLine2"); }
+
+		///<summary>
+		/// Closing Time: Please provide the day when you are not open
+		///</summary>
+		[ImplementPropertyType("closingTime")]
+		public string ClosingTime
+		{
+			get { return GetClosingTime(this); }
+		}
+
+		/// <summary>Static getter for Closing Time</summary>
+		public static string GetClosingTime(IContactInfo that) { return that.GetPropertyValue<string>("closingTime"); }
+
+		///<summary>
+		/// Opening Times: Please provide your opening times
+		///</summary>
+		[ImplementPropertyType("openingTimes")]
+		public string OpeningTimes
+		{
+			get { return GetOpeningTimes(this); }
+		}
+
+		/// <summary>Static getter for Opening Times</summary>
+		public static string GetOpeningTimes(IContactInfo that) { return that.GetPropertyValue<string>("openingTimes"); }
+
+		///<summary>
+		/// Phone number: Please provide your phone number
+		///</summary>
+		[ImplementPropertyType("phoneNumber")]
+		public string PhoneNumber
+		{
+			get { return GetPhoneNumber(this); }
+		}
+
+		/// <summary>Static getter for Phone number</summary>
+		public static string GetPhoneNumber(IContactInfo that) { return that.GetPropertyValue<string>("phoneNumber"); }
+
+		///<summary>
+		/// Postcode: Please provide your Postcode
+		///</summary>
+		[ImplementPropertyType("postcode")]
+		public string Postcode
+		{
+			get { return GetPostcode(this); }
+		}
+
+		/// <summary>Static getter for Postcode</summary>
+		public static string GetPostcode(IContactInfo that) { return that.GetPropertyValue<string>("postcode"); }
+
+		///<summary>
+		/// Text for phone number: Please add some text to you phone number
+		///</summary>
+		[ImplementPropertyType("textForPhoneNumber")]
+		public string TextForPhoneNumber
+		{
+			get { return GetTextForPhoneNumber(this); }
+		}
+
+		/// <summary>Static getter for Text for phone number</summary>
+		public static string GetTextForPhoneNumber(IContactInfo that) { return that.GetPropertyValue<string>("textForPhoneNumber"); }
 	}
 
 	/// <summary>Folder</summary>
