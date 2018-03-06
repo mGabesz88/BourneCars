9f786e40b23bd361d47e0bd91a62606ceba55c6f
Gabor Molnar <gabormolnar@redweb.com>
2018-03-06 18:54:16 +0000

    Created new finance page and did some bug fixing.

---
+++ Controllers/FinancePageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace BourneCars.Controllers
+{
+    public class FinancePageController : SurfaceController
+    {
+        public const string FinancePartialViewFolder = "~/Views/Partials/Finance";
+        public ActionResult RenderFinanceDetails()
+        {
+            return PartialView(FinancePartialViewFolder + "/_FinanceDetails.cshtml");
+        }
+
+        public ActionResult RenderContactUsDetails()
+        {
+            return PartialView(FinancePartialViewFolder + "/_ContactUsDetails.cshtml");
+        }
+    }
+}
